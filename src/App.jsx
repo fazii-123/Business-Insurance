eaa6f156580eb108b046143f8a9d3e1254c33685
legendxfaizan <ffaizan0870@gmail.com>
2025-08-26 03:26:03 -0700

    Commits

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <Route path="/business-insurance" element={<BusinessInsurance />} />
           <Route path="/taxi-insurance" element={<TaxiInsurance />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/agents/agent1" element={<Agent1 />} /> 
+          <Route path="/agent1" element={<Agent1 />} /> 
         </Routes>
       </main>
     </div>
